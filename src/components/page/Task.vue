78b4c71f711830232d1897947813bdd4c290f20f
Viarow <37031276+Viarow@users.noreply.github.com>
2018-07-13 16:51:40 +0800

    Update Task.vue

--- src/components/page/Task.vue
+++ src/components/page/Task.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+
+      <!--作业要求-->
       <div>
       <el-collapse v-model="activeNames" @change="handleChange">
         <el-collapse-item title="  作业要求" name="1" >
@@ -14,6 +16,69 @@
       </el-collapse>
       </div>
 
+      <!--班级成员-->
+      <div>
+        <p class="submitted">班级成员</p>
+        <el-table
+          :data="memberTable"
+          style="width: 100%"
+          max-height="400">
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            prop="number"
+            label="学号"
+            width="300">
+          </el-table-column>
+          <el-table-column
+            prop="status"
+            label="状态"
+            width="150">
+          </el-table-column>
+
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="400">
+            <template slot-scope="scope">
+              <el-button
+                @click.native.prevent="deleteRow(scope.$index, memberTable)"
+                type="text"
+                size="small">
+                移除
+              </el-button>
+              <!--<el-button type="text" @click="dialogFormVisible = true">点击添加新成员</el-button>-->
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-button type="text" @click="dialogFormVisible = true" class="newMember">点击添加新成员</el-button>
+      </div>
+
+      <!--添加新的班级成员-->
+      <div>
+
+        <el-dialog title="添加新成员" :visible.sync="dialogFormVisible">
+          <el-form :model="form">
+            <el-form-item label="姓名" >
+              <el-input v-model="form.name" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="学号" >
+              <el-input v-model="form.number" auto-complete="off"></el-input>
+            </el-form-item>
+          </el-form>
+
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="addTask(dialogFormVisible,form)">确 定</el-button>
+          </div>
+
+        </el-dialog>
+      </div>
+
+      <!--已交名单-->
       <div>
         <p class="submitted">已交名单</p>
         <el-table  :data="submitTable"  style="width: 100%"  max-height="400">
@@ -48,6 +113,7 @@
 
       </div>
 
+      <!--未交名单-->
       <div>
         <p class="submitted">未交名单</p>
         <el-table :data="delayTable" style="width: 100%" max-height="400">
@@ -85,6 +151,25 @@
       data() {
         return {
           activeNames: ['2'],
+
+          memberTable:[{
+            name:'大雄',
+            number:'PB16060143',
+            status:'已加入'
+          },{
+            name:'静香',
+            number:'PB16060142',
+            status:'已加入'
+          },{
+            name:'胖虎',
+            number:'PB16060141',
+            status:'已加入'
+          }],
+          dialogFormVisible: false,
+          form:{
+            name:' ',
+            number:' '
+          },
           submitTable:[{
             name:'wolfgang',
             number:'PB16060457',
@@ -98,14 +183,17 @@
           }],
 
           delayTable:[{
-            name:'wolfgang',
-            number:'PB16060457',
+            name:'大雄',
+            number:'PB16060143',
           },{
-            name:'kala',
-            number:'PB17060178',
+            name:'静香',
+            number:'PB16060142',
+          },{
+            name:'胖虎',
+            number:'PB16060141',
           }]
 
-        };
+        }
       },
 
       methods: {
@@ -128,6 +216,15 @@
             //   });
             // }
           });
+        },
+        addTask(dialogFormVisible,form){
+          this.dialogFormVisible = false;
+          let obj={
+            name:form.name,
+            number:form.number,
+            status:'未加入'
+          }
+          this.memberTable.push(obj);
         }
       }
 
@@ -135,5 +232,9 @@
 </script>
 
 <style scoped>
-
+   .newMember{
+     position:relative;
+     left:1000px;
+     top:0px
+   }
 </style>
